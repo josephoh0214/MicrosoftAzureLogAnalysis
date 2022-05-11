016aaae876767d97d72cbdb173b52b509e456f0a
josephoh0214 <josephoh0214@gmail.com>
2022-05-11 14:34:33 -0700

    fixed image slider section

--- src/contoso_slider.js
+++ src/contoso_slider.js
@@ -2,11 +2,11 @@ import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
-import contoso1 from "./viz/contoso1.jpg"
-import contoso2 from "./viz/contoso2.jpg"
-import contoso3 from "./viz/contoso3.jpg"
-import contoso4 from "./viz/contoso4.jpg"
-import contoso5 from "./viz/contoso5.jpg"
+import contoso1 from "./viz/contoso-1.jpg"
+import contoso2 from "./viz/contoso-2.jpg"
+import contoso3 from "./viz/contoso-3.jpg"
+import contoso4 from "./viz/contoso-4.jpg"
+import contoso5 from "./viz/contoso-5.jpg"
 
 
 export function ContosoSlider({width, height}) {
--- src/pipeline_slider.js
+++ src/pipeline_slider.js
@@ -2,13 +2,13 @@ import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 
-import pipeline1 from "./viz/pipeline1.jpg"
-import pipeline2 from "./viz/pipeline2.jpg"
-import pipeline3 from "./viz/pipeline3.jpg"
-import pipeline4 from "./viz/pipeline4.jpg"
-import pipeline5 from "./viz/pipeline5.jpg"
-import pipeline6 from "./viz/pipeline6.jpg"
-import pipeline7 from "./viz/pipeline7.jpg"
+import pipeline1 from "./viz/pipeline-1.jpg"
+import pipeline2 from "./viz/pipeline-2.jpg"
+import pipeline3 from "./viz/pipeline-3.jpg"
+import pipeline4 from "./viz/pipeline-4.jpg"
+import pipeline5 from "./viz/pipeline-5.jpg"
+import pipeline6 from "./viz/pipeline-6.jpg"
+import pipeline7 from "./viz/pipeline-7.jpg"
 
 
 export function PipelineSlider({width, height}) {
